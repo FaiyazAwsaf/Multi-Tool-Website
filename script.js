9c48fb6be6909be9c11f1d8184e46df9b2a91c55
Faiyaz A Awsaf <faiyazawsaf11@gmail.com>
2024-06-21 02:33:33 +0600

    minor JS changes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-document.getElementById('search-btn').addEventListener('click', function() {
+
+
+
+function displayWeather(){
     const city = document.getElementById('city-input').value;
     const apiKey = '1379f41b1aa7a0cf6db28de6974844b5'; 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            console.log('API Response:', data);
             if (data.cod === 200) {
                 const windSpeed = data.wind.speed;
                 const temp = Math.round(data.main.temp); 
@@ -18,21 +19,31 @@ document.getElementById('search-btn').addEventListener('click', function() {
                 const feelsLike = data.main.feels_like;
 
 
-                document.getElementById('temp').innerText = `${temp}°C`;
+                document.getElementById('temp').innerText = temp + "°C";
                 document.getElementById('weatherSummary').innerText = `${capitalizeFirstLetter(weatherSummary)}`;
                 document.getElementById('humidity').innerText = `${humidity}%`;
-                document.getElementById('windSpeed').innerText = `${windSpeed} km/h`;
+                document.getElementById('windSpeed').innerText = `${windSpeed} m/s`;
                 document.getElementById('location').innerText = `${location}`;
-                document.getElementById('airPressure').innerText = `${airPressure}`;
-                document.getElementById('feelsLike').innerText = `${feelsLike}`;
+                document.getElementById('airPressure').innerText = `${airPressure} Pa`;
+                document.getElementById('feelsLike').innerText = `${feelsLike} °C`;
+
+                
+
             } else {
                 alert('City not found');
             }
         })
         .catch(error => console.error('Error fetching data:', error));
-});
-
-
+        console.log('API Response:', data);
+}
+    
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+document.getElementById('search-btn').addEventListener('click', function() {
+    displayWeather();
+});
+
+
+
